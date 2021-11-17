cded628cf4d42f5669764c2167e14cd6fd74351a
Sandor <77490720+sandor887@users.noreply.github.com>
2021-11-17 10:52:02 +0100

    Add files via upload

---
+++ feladta.py
@@ -0,0 +1,13 @@
+import random
+list = ["Bea", "Zoli", "Béla", "Gáspár", "Irén"]
+nev = input('Adj meg egy nevet: ')
+if nev in list:
+     print('Benne van')
+else:
+    print('Nincs benne')
+
+szam = ['1', '3', '5', '8', '11']
+
+
+szam1 = random.randint(10,30)
+print()
